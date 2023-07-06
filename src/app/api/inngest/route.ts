c72decb980d4177dacbe03dabb3a132d236f9065
Igor Gassmann <igor@inngest.com>
2023-07-06 17:36:05 +0200

    fix: add missing Inngest func to serve

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,5 +1,9 @@
 import processCSVFile from '@/app/use-cases/background-jobs/csv-file/processCSVFile';
+import customerOnboarding from '@/app/use-cases/workflows/onboarding/customerOnboarding';
 import inngest from './client';
 import { serve } from "inngest/next";
 
-export const { GET, POST, PUT } = serve(inngest, [processCSVFile]);
+export const { GET, POST, PUT } = serve(inngest, [
+  processCSVFile,
+  customerOnboarding
+]);
