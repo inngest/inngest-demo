2b3866b4ad6b600b3181a6820683ed0392351774
Igor Gassmann <igor@inngest.com>
2023-07-07 10:49:20 +0200

    feat: update homepage copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="justify-between max-w-7xl mx-auto py-24 sm:px-2 sm:py-32 lg:px-4 space-y-24">
-      <div className="mx-auto max-w-2xl lg:text-center">
+      <div className="mx-auto max-w-3xl lg:text-center space-y-4">
         <h1 className="text-3xl lg:text-5xl font-semibold">Use Cases</h1>
+        <p className="[text-wrap:balance] text-sm md:text-base text-indigo-200">
+          Inngest provides you all the tools that you need to build powerful applications within your existing codebase.
+        </p>
       </div>
       <div className="grid gap-4 grid-cols-3 w-full">
         <Link href="/use-cases/background-jobs" className="flex flex-col w-full h-[380px] p-6 overflow-hidden border hover:bg-gradient-to-b hover:from-[#050911] hover:from-70% hover:to-slate-950 hover:to-100% rounded-xl bg-[#050911] border-zinc-800 transition-colors duration-150 hover:border-slate-700">
@@ -12,21 +15,21 @@ export default function Home() {
             {/* Illustration */}
           </div>
           <h2 className="text-xl font-semibold">Background Jobs</h2>
-          <p className="text-sm text-zinc-500">Run background jobs with ease</p>
+          <p className="text-sm text-zinc-500">Run reliable background jobs without deploying any new infrastructure.</p>
         </Link>
         <Link href="/use-cases/workflows" className="flex flex-col w-full h-[380px] p-6 overflow-hidden border hover:bg-gradient-to-b hover:from-[#050911] hover:from-70% hover:to-slate-950 hover:to-100% rounded-xl bg-[#050911] border-zinc-800 transition-colors duration-150 hover:border-slate-700">
           <div className="h-full">
             {/* Illustration */}
           </div>
           <h2 className="text-xl font-semibold">Workflows</h2>
-          <p className="text-sm text-zinc-500">Build complex workflows in minutes</p>
+          <p className="text-sm text-zinc-500">Build complex workflows in minutes without file configurations.</p>
         </Link>
         <Link href="/use-cases/scheduled-functions" className="flex flex-col w-full h-[380px] p-6 overflow-hidden border hover:bg-gradient-to-b hover:from-[#050911] hover:from-70% hover:to-slate-950 hover:to-100% rounded-xl bg-[#050911] border-zinc-800 transition-colors duration-150 hover:border-slate-700">
           <div className="h-full">
             {/* Illustration */}
           </div>
           <h2 className="text-xl font-semibold">Scheduled Functions</h2>
-          <p className="text-sm text-zinc-500">Run functions on a schedule</p>
+          <p className="text-sm text-zinc-500">Run functions on a schedule that automatically retries on failure.</p>
         </Link>
       </div>
     </div>
