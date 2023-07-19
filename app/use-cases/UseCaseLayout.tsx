6a4bc216e72785addd51ad83550f44e342dc7f56
Igor Gassmann <igor@inngest.com>
2023-07-19 17:02:48 +0200

    refactor: extract UseCaseLayout

---
+++ app/use-cases/UseCaseLayout.tsx
@@ -0,0 +1,16 @@
+export default function UseCaseLayout({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title: string;
+}) {
+  return (
+    <div className="mx-auto max-w-7xl py-24 sm:px-2 sm:py-32 lg:px-4 space-y-24">
+      <div className="mx-auto max-w-2xl px-4 lg:text-center">
+        <h1 className="text-4xl font-bold">{title}</h1>
+      </div>
+      <div className="mx-auto max-w-2xl px-4 lg:max-w-none">{children}</div>
+    </div>
+  );
+}
