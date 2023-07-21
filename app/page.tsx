33ea494864d4f853b492751418f7635e9eac3b6c
Igor Gassmann <igor@inngest.com>
2023-07-21 11:36:07 +0200

    refactor: extract UseCaseCategoryCard

--- app/page.tsx
+++ app/page.tsx
@@ -12,37 +12,48 @@ export default function Home() {
         </p>
       </hgroup>
       <div className="px-4 grid gap-4 grid-cols-1 max-w-md xl:grid-cols-3 xl:max-w-none w-full mx-auto">
-        <Link
-          href="/background-jobs"
-          className="flex flex-col w-full h-[380px] p-6 overflow-hidden border hover:bg-gradient-to-b hover:from-[#050911] hover:from-70% hover:to-slate-950 hover:to-100% rounded-xl bg-[#050911] border-zinc-800 transition-colors duration-150 hover:border-slate-700"
-        >
-          <div className="h-full">{/* Illustration */}</div>
-          <h2 className="text-xl font-semibold">Background Jobs</h2>
-          <p className="text-sm text-zinc-500">
-            Run reliable background jobs without deploying any new infrastructure.
-          </p>
-        </Link>
-        <Link
-          href="/workflows"
-          className="flex flex-col w-full h-[380px] p-6 overflow-hidden border hover:bg-gradient-to-b hover:from-[#050911] hover:from-70% hover:to-slate-950 hover:to-100% rounded-xl bg-[#050911] border-zinc-800 transition-colors duration-150 hover:border-slate-700"
-        >
-          <div className="h-full">{/* Illustration */}</div>
-          <h2 className="text-xl font-semibold">Workflows</h2>
-          <p className="text-sm text-zinc-500">
-            Build complex workflows in minutes without file configurations.
-          </p>
-        </Link>
-        <Link
-          href="/scheduled-functions"
-          className="flex flex-col w-full h-[380px] p-6 overflow-hidden border hover:bg-gradient-to-b hover:from-[#050911] hover:from-70% hover:to-slate-950 hover:to-100% rounded-xl bg-[#050911] border-zinc-800 transition-colors duration-150 hover:border-slate-700"
-        >
-          <div className="h-full">{/* Illustration */}</div>
-          <h2 className="text-xl font-semibold">Scheduled Functions</h2>
-          <p className="text-sm text-zinc-500">
-            Run functions on a schedule that automatically retries on failure.
-          </p>
-        </Link>
+        <UseCaseCategoryCard
+          name="Background Jobs"
+          description="Run reliable background jobs without deploying any new infrastructure."
+          illustration={<></>}
+          slug="background-jobs"
+        />
+        <UseCaseCategoryCard
+          name="Workflows"
+          description="Build complex workflows in minutes without file configurations."
+          illustration={<></>}
+          slug="workflows"
+        />
+        <UseCaseCategoryCard
+          name="Scheduled Functions"
+          description="Run functions on a schedule that automatically retries on failure."
+          illustration={<></>}
+          slug="scheduled-functions"
+        />
       </div>
     </div>
+  );
+}
+
+function UseCaseCategoryCard({
+  name,
+  description,
+  illustration,
+  slug,
+}: {
+  name: string;
+  description: string;
+  illustration: React.ReactNode;
+  slug: string;
+}) {
+  return (
+    <Link
+      href={`/${slug}`}
+      className="flex flex-col w-full h-[380px] p-6 overflow-hidden border hover:bg-gradient-to-b hover:from-[#050911] hover:from-70% hover:to-slate-950 hover:to-100% rounded-xl bg-[#050911] border-zinc-800 transition-colors duration-150 hover:border-slate-700"
+    >
+      <div className="h-full">{illustration}</div>
+      <h2 className="text-xl font-semibold">{name}</h2>
+      <p className="text-sm text-zinc-500">{description}</p>
+    </Link>
   );
 }
