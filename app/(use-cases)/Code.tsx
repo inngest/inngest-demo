69db9f41fd86e6f26db1e22f4a37c5b65b89e10a
Igor Gassmann <igor@inngest.com>
2023-07-25 15:48:44 +0200

    feat: trim code

--- app/(use-cases)/Code.tsx
+++ app/(use-cases)/Code.tsx
@@ -5,9 +5,13 @@ export default async function Code({ fileURL }: { fileURL: URL }) {
 
   const fileContent = await fetch(fileURL).then((res) => res.text());
 
+  const startString = "inngest.createFunction(";
+
+  const inngestFunctionCode = fileContent.substring(fileContent.indexOf(startString)).trimEnd();
+
   return (
     <SyntaxHighlighter style={{margin: 0}} className="text-mono text-xs" theme={theme} lang="ts">
-      {fileContent}
+      {inngestFunctionCode}
     </SyntaxHighlighter>
   );
 }
