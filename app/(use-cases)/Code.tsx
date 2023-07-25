165e014741907b6bfb0f96a0f295b9fcad57ccb5
Igor Gassmann <igor@inngest.com>
2023-07-25 12:09:58 +0200

    fix: code layout

--- app/(use-cases)/Code.tsx
+++ app/(use-cases)/Code.tsx
@@ -6,7 +6,7 @@ export default async function Code({ fileURL }: { fileURL: URL }) {
   const fileContent = await fetch(fileURL).then((res) => res.text());
 
   return (
-    <SyntaxHighlighter className="!-my-[1em] text-mono text-xs" theme={theme} lang="ts">
+    <SyntaxHighlighter style={{margin: 0}} className="text-mono text-xs" theme={theme} lang="ts">
       {fileContent}
     </SyntaxHighlighter>
   );
