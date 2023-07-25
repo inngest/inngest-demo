6c0bf542321e4a590ae1e97fa8493f8d985da9f6
Igor Gassmann <igor@inngest.com>
2023-07-25 11:32:49 +0200

    fix: can't load code in prod

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-const path = require('node:path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: true,
     // This makes sure to include inngest functions in the server bundle
-    outputFileTracingRoot: path.join(__dirname, './app/(use-cases)/'),
+    outputFileTracingIncludes: {
+      '/background-jobs': ['./app/(use-cases)/background-jobs/**/*'],
+      '/workflows': ['./app/(use-cases)/workflows/**/*'],
+      '/scheduled-functions': ['./app/(use-cases)/scheduled-functions/**/*'],
+    },
   }
 };
 
