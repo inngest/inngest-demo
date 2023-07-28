c7d9b5633f8e78f21f32d7e253961bbbe0eb81fa
Igor Gassmann <igor@inngest.com>
2023-07-28 16:30:01 +0200

    feat: remove artificial delays in actions

--- app/(use-cases)/background-jobs/csv-file-import/action.ts
+++ app/(use-cases)/background-jobs/csv-file-import/action.ts
@@ -4,9 +4,6 @@ import inngest from '@/app/api/inngest/client';
 
 export default async function action() {
   async function fakeUpload() {
-    // Simulate upload time
-    await new Promise((r) => setTimeout(r, 2_000));
-
     return {
       url: '/public/500-contacts.csv',
       uploadedAt: new Date(),
--- app/(use-cases)/workflows/customer-onboarding/action.ts
+++ app/(use-cases)/workflows/customer-onboarding/action.ts
@@ -3,9 +3,6 @@
 import inngest from '@/app/api/inngest/client';
 
 export default async function action() {
-  // Artificial delay to simulate a network request
-  await new Promise((r) => setTimeout(r, 1_000));
-
   await inngest.send({
     name: 'app/user.signed.up',
     data: {
