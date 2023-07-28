9c517ca93475b0e204bf9c37189d40a405cd0ccc
Igor Gassmann <igor@inngest.com>
2023-07-28 16:33:15 +0200

    feat: hide illustration tab until we have designed them

--- app/(use-cases)/Demo.tsx
+++ app/(use-cases)/Demo.tsx
@@ -17,14 +17,14 @@ export default function Demo({
       as="aside"
       className="lg:col-span-3 relative min-h-0 p-1 flex-1 rounded-lg border border-dashed border-zinc-800 bg-[#050911]"
     >
-      <div className="pointer-events-none absolute inset-x-0 top-0 right-2 z-10 m-0.5 md:left-auto">
-        <Tab.List className="flex items-stretch justify-end rounded-t-[10px] px-3 py-1 md:m-1 md:rounded-lg space-x-1">
-          <Tab className={tabClasses}>Illustration</Tab>
-          <Tab className={tabClasses}>Code</Tab>
-        </Tab.List>
-      </div>
+      {/*<div className="pointer-events-none absolute inset-x-0 top-0 right-2 z-10 m-0.5 md:left-auto">*/}
+      {/*  <Tab.List className="flex items-stretch justify-end rounded-t-[10px] px-3 py-1 md:m-1 md:rounded-lg space-x-1">*/}
+      {/*    <Tab className={tabClasses}>Illustration</Tab>*/}
+      {/*    <Tab className={tabClasses}>Code</Tab>*/}
+      {/*  </Tab.List>*/}
+      {/*</div>*/}
       <Tab.Panels className="h-full overflow-auto">
-        <Tab.Panel>{illustration}</Tab.Panel>
+        {/*<Tab.Panel>{illustration}</Tab.Panel>*/}
         <Tab.Panel className="bg-slate-900 h-full">{code}</Tab.Panel>
       </Tab.Panels>
     </Tab.Group>
