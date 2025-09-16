5a946d9194f2f4874d61ffa16abc9f67262b835f
zasghar26 <151073341+zasghar26@users.noreply.github.com>
2025-09-16 14:58:02 +0500

    Update route.ts

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,4 +1,9 @@
-import { inngest, functions } from '@/inngest';
-import { serve } from 'inngest/next';
+// app/api/inngest/route.ts
+import { serve } from "inngest/next";
+import { inngest } from "../../../inngest/client";   // your client
+import * as fns from "../../../inngest/functions";   // your functions
 
-export const { GET, POST, PUT } = serve({ client: inngest, functions});
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: Object.values(fns),
+});
